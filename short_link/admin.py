cab7df573291a57412e33eb94adb76554f2ad8fa
kengenal <kengenal1@gmail.com>
2022-05-03 12:53:07 +0200

    Add Link model and LinkStats model to admin

--- short_link/admin.py
+++ short_link/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from short_link.models import Links, LinkStats
+
+
+@admin.register(Links)
+class LinkAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(LinkStats)
+class LinkStatsAdmin(admin.ModelAdmin):
+    pass
