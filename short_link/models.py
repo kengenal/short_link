1868427a894b44cab953768a596e4bf000f946be
kengenal <kengenal1@gmail.com>
2022-05-03 23:42:38 +0200

    Change fields names in Link model

--- short_link/models.py
+++ short_link/models.py
@@ -1,23 +1,25 @@
-from django.conf import settings
 from django.db import models
-from django.utils.crypto import get_random_string
 
 class DateModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Links(DateModel):
-    link_id = models.CharField(
+    new_url = models.CharField(
         max_length=120,
         unique=True,
-        default=get_random_string(settings.SHORT_LINK_LENGTH),
         blank=True,
         null=True
     )
-    main_url = models.URLField()
+    url = models.URLField(
+        unique=True,
+        blank=False,
+        null=False,
+        default=None,
+    )
 
     def __str__(self):
-        return str(self.link_id)
+        return str(self.new_url)
 
 class LinkStats(DateModel):
     link = models.ForeignKey("Links", on_delete=models.CASCADE)
@@ -25,4 +27,4 @@ class LinkStats(DateModel):
     user_agent = models.CharField(max_length=120)
 
     def __str__(self):
-        return str(self.link.link_id)
+        return str(self.link.new_url)
