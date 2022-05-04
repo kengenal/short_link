88d58baaaf5f1f7fa0cd18c33fce3dd9652ba660
kengenal <kengenal1@gmail.com>
2022-05-04 12:34:49 +0200

    Create serializer for ShortLinkGenerateApiView

---
+++ short_link/api/serializers.py
@@ -0,0 +1,28 @@
+from django.urls import reverse
+from rest_framework import serializers
+
+from short_link.models import Links
+
+
+class ShortLinkSerializer(serializers.ModelSerializer):
+    link = serializers.SerializerMethodField(read_only=True)
+    url = serializers.URLField(required=True)
+
+    class Meta:
+        model = Links
+
+        read_only_fields = ['new_url']
+        fields = ['url', 'link'] + read_only_fields
+
+    def get_link(self, obj):
+        return self.context.get(
+            'request'
+        ).build_absolute_uri(
+            reverse('redirect-view', kwargs={'new_url': obj.new_url})
+        )
+
+    def create(self, validated_data):
+        model = self.Meta.model.objects.filter(url=validated_data.get('url'))
+        if model.exists():
+            return model.first()
+        return super().create(validated_data)
