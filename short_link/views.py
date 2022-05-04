34fde3aadd27773414240cecf66d06681add3aef
kengenal <kengenal1@gmail.com>
2022-05-04 13:12:26 +0200

    Add stats for Redirect View

--- short_link/views.py
+++ short_link/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import HttpResponseRedirect
 from django.views.generic import DetailView
 
-from short_link.models import Links
+from short_link.models import Links, LinkStats
 
 
 class RedirectView(DetailView):
     model = Links
+    stats_model = LinkStats
     slug_url_kwarg = 'new_url'
     slug_field = 'new_url'
 
     def get(self, request, *args, **kwargs):
         super().get(request, *args, **kwargs)
+        self.stats()
         return HttpResponseRedirect(redirect_to=self.object.url)
+
+    def stats(self):
+        self.stats_model.objects.create(
+            ip=self.request.META.get("REMOTE_ADDR"),
+            user_agent=self.request.META.get('HTTP_USER_AGENT'),
+            link=self.object
+        )
